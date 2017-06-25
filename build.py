1bef0268a2d138136b8ac8342b9365220bf1f1a3
nixen420 <nikhil.singh4008@gmail.com>
2017-06-25 17:10:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-
+ds1=[2, 4, 6, 8, 10]
+ds2=[1, 3, 5, 7, 10]
 def equal_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return (ds1==ds2)
 
 
 def greater_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
-
+    return (ds1>ds2)
 
 def less_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return (ds1<ds2)
+
+equal_operator(ds1, ds2)
+greater_than_operator(ds1, ds2)
+less_than_operator(ds1, ds2)
